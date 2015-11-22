6f6177ddf2b3ee0fb18171bcfae32e9016420fe3
Emil Thidell <mail@emil.ninja>
2015-11-22 20:20:28 +0100

    Autopublish removed

--- start.bash
+++ start.bash
@@ -79,6 +79,7 @@ if [ "${TYPE}" == "node" ]; then
         mkdir -p tests/jasmine/server/unit
         mkdir -p tests/jasmine/client/integration
         meteor add sanjo:jasmine
+        meteor add velocity:html-reporter
         meteor remove autopublish
         meteor" > /tmp/install
     fi
