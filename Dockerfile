a962e6225fc511851cb6845922cc52210f4fed09
Emil Thidell <mail@emil.ninja>
2015-11-24 13:23:46 +0100

    nodever added to dockerfile

--- Dockerfile
+++ Dockerfile
@@ -20,23 +20,19 @@
 # FROM Baseimage ubuntu
 FROM ubuntu
 
-# ENV-TYPE: node/php
-ENV TYPE node
-
-# ENV-FRAMEWORK: wordpress/laravel/phalcon/meteor/sails/mean
+# ENV-FRAMEWORK: meteor/sails/mean
 ENV FRAMEWORK meteor
 
+ENV NODEVER 0.12.6
+
 # ENV-FRAMEWORKVER: 1.0
-ENV FRAMEWORKVER 1.2.1
+ENV FRAMEWORKVER 1.0
 
 # ENV-FRAMEWORKPACKS: <frameworks pack install script>
 ENV FRAMEWORKPACK "meteor add iron:router meteor add fourseven:scss@2.0.0 meteor add meteoric:ionic-sass meteor add meteoric:ionicons-sass meteor add meteoric:ionic"
 
 # ENV-APPNAME
 ENV APPNAME testapp
-
-# HTTPD: apache/nginx
-ENV HTTPD apache
 
 # ENV-LOCALE
 ENV LOCALE en_US.UTF-8
@@ -53,6 +49,19 @@ RUN chmod +x /start
 
 # Expose ports. Ghost,Meteor:3000 Sails:1337
 EXPOSE 3000 1337
+
+RUN apt-get -y update && apt-get -y install \
+    wget \
+    git \
+    python \
+    make \
+    build-essential
+RUN wget https://nodejs.org/dist/v$NODEVER/node-v$NODEVER-linux-x64.tar.gz && \
+    tar -zxf node-v$NODEVER-linux-x64.tar.gz -C /usr/local && \
+    ln -sf node-v$NODEVER-linux-x64 /usr/local/node && \
+    ln -sf /usr/local/node/bin/npm /usr/local/bin/ && \
+    ln -sf /usr/local/node/bin/node /usr/local/bin/ && \
+    rm node-v$NODEVER-linux-x64.tar.gz
 
 # Start the install
 CMD /start --framework=$FRAMEWORK --type=$TYPE --appname=$APPNAME --locale=$LOCALE
--- start.bash
+++ start.bash
@@ -2,10 +2,6 @@
 for i in "$@"
 do
 case $i in
-    -t=*|--type=*)
-    TYPE="${i#*=}"
-    shift # past argument=value
-    ;;
     -l=*|--locale=*)
     LOCALE="${i#*=}"
     shift # past argument=value
@@ -34,63 +30,40 @@ done
 
 locale-gen ${LOCALE}
 
-if [ "${TYPE}" == "php" ]; then
-
-    apt-get update
-    apt-get install -y python-software-properties
-    apt-get update
-    apt-get install -y php5
-
-    if [ "${FRAMEWORK}" == "laravel" ]; then
-        echo "Framework: Laravel"
-    fi
-    if [ "${FRAMEWORK}" == "wordpress" ]; then
-        echo "Framework: Wordpress"
-    fi
-
+if [ "${FRAMEWORK}" == "mean" ]; then
     echo "#!/bin/bash
-        set -e
-        rm -f /var/run/apache2/apache2.pid
-        exec apache2 -DFOREGROUND" > /webmaker/run
-    fi
-if [ "${TYPE}" == "node" ]; then
-
-
-    if [ "${FRAMEWORK}" == "mean" ]; then
+    npm install -g bower grunt-cli
+    cd /webmaker
+    git clone https://github.com/meanjs/mean.git ${APPNAME}
+    cd ${APPNAME}
+    npm install
+    bower --allow-root --config.interactive=false install
+    grunt" > /webmaker/run
+fi
+if [ "${FRAMEWORK}" == "sails" ]; then
+    echo "#!/bin/bash
+    npm -g install sails
+    cd /webmaker
+    sails new ${APPNAME}
+    cd ${APPNAME}
+    sails lift" > /webmaker/run
+fi
+if [ "${FRAMEWORK}" == "meteor" ]; then
+    if [ ! -d "/webmaker/${APPNAME}" ]; then
         echo "#!/bin/bash
-        npm install -g bower grunt-cli
         cd /webmaker
-        git clone https://github.com/meanjs/mean.git ${APPNAME}
+        /usr/bin/curl https://install.meteor.com/ | /bin/sh
+        mkdir ${APPNAME}
+        cp -r /tmp/app/* /webmaker/${APPNAME}
         cd ${APPNAME}
-        npm install
-        bower --allow-root --config.interactive=false install
-        grunt" > /webmaker/run
-    fi
-    if [ "${FRAMEWORK}" == "sails" ]; then
+        ${FRAMEWORKPACK}
+        meteor" > /webmaker/run
+    else
         echo "#!/bin/bash
-        npm -g install sails
         cd /webmaker
-        sails new ${APPNAME}
-        cd ${APPNAME}
-        sails lift" > /webmaker/run
-    fi
-    if [ "${FRAMEWORK}" == "meteor" ]; then
-        if [ ! -d "/webmaker/${APPNAME}" ]; then
-            echo "#!/bin/bash
-            cd /webmaker
-            /usr/bin/curl https://install.meteor.com/ | /bin/sh
-            mkdir ${APPNAME}
-            cp -r /tmp/app/* /webmaker/${APPNAME}
-            cd ${APPNAME}
-            ${FRAMEWORKPACK}
-            meteor" > /webmaker/run
-        else
-            echo "#!/bin/bash
-            cd /webmaker
-            /usr/bin/curl https://install.meteor.com/ | /bin/sh
-            cd /webmaker/${APPNAME}
-            meteor" > /webmaker/run
-        fi
+        /usr/bin/curl https://install.meteor.com/ | /bin/sh
+        cd /webmaker/${APPNAME}
+        meteor" > /webmaker/run
     fi
 fi
 
