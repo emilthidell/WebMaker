9c83bbf2ea4a73a80374fb35d5d6a9a16c90a344
Emil Thidell <mail@emil.ninja>
2015-11-26 19:59:18 +0100

    push

--- app/prebuild/scripts/prebuild.bash
+++ app/prebuild/scripts/prebuild.bash
@@ -6,5 +6,8 @@ mkdir .meteor/local/build/programs
 mkdir .meteor/local/build/programs/server
 mkdir .meteor/local/build/programs/server/assets
 mkdir .meteor/local/build/programs/server/assets/packages
-cp -r prebuild/resources/meteoric_ionic-sass .meteor/local/build/programs/server/assets/packages
-cp -r prebuild/resources/meteoric_ionicons-sass .meteor/local/build/programs/server/assets/packages
+mkdir .meteor/local/build/programs/server/assets/packages/meteoric_ionic-sass
+mkdir .meteor/local/build/programs/server/assets/packages/meteoric_ionicons-sass
+
+cp -r prebuild/resources/meteoric_ionic-sass/* .meteor/local/build/programs/server/assets/packages/meteoric_ionic-sass
+cp -r prebuild/resources/meteoric_ionicons-sass/* .meteor/local/build/programs/server/assets/packages/meteoric_ionicons-sass
